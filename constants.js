3b8b66d6bac4ca25a84e48178b1f9b9e8df574dd
mac <mac@TanWeideMacBook-Pro.local>
2018-06-05 17:38:25 +0800

    变量重新定义

--- constants.js
+++ constants.js
@@ -5,46 +5,51 @@ const VERSION = '1.0';
 const PATHS = process.env.PATHS || '';
 
 // 秘钥
-exports.PASSWORD = {
+const PASSWORD = {
     prod: 'XwxQ7i4T65BVaPqeX2Bc0XxSMHYGWuP6',
     sandbox: 'ZDqKURwRpSj070WjujeSaZfJsnh0ETk7',
     test: 'sY0KVp5jLtYDtpFquh29Nuc0lAJOQOsF',
     dev: 'YpVlYlPHOzDWSBKoEoDPoQirHvBc4rnr',
-}[exports.JIMI_ENV];
+}[JIMI_ENV];
 
 // rbac接口
-exports.API_RBAC = {
+const API_RBAC = {
     dev: 'http://rbac-api.dev.jimistore.com',
     test: 'http://rbac-api.test.jimistore.com',
     prod: 'http://rbac-api.jimistore.com',
     sandbox: 'https://user-api.jimistore.com',
-}[exports.JIMI_ENV];
+}[JIMI_ENV];
 
 // 平台接口
-exports.API = {
+const API = {
     dev: 'http://platform-risk.dev.jimistore.com',
     test: 'http://platform-risk.test.jimistore.com',
     sandbox: 'https://testmsp-api.jimistore.com',
     prod: 'https://msp-api.jimistore.com',
-}[exports.JIMI_ENV];
+}[JIMI_ENV];
 
 // 公用
-exports.API_COMMON = {
+const API_COMMON = {
     dev: 'http://api.test.jimistore.com:4999',
     test: 'http://api.test.jimistore.com:4999',
     sandbox: 'https://testcommon.jimistore.com',
     prod: 'https://common.jimistore.com',
-}[exports.JIMI_ENV];
+}[JIMI_ENV];
 
 // 用户
-exports.API_USER = {
+const API_USER = {
     dev: 'http://user.dev.jimistore.com',
     test: 'http://user.test.jimistore.com',
     sandbox: 'https://testcommon.jimistore.com',
     prod: 'https://user-api.jimistore.com',
-}[exports.JIMI_ENV];
+}[JIMI_ENV];
 
 exports.PORT = PORT;
 exports.JIMI_ENV = JIMI_ENV;
 exports.PATHS = PATHS;
 exports.VERSION = VERSION;
+exports.API = API;
+exports.API_COMMON = API_COMMON;
+exports.API_USER = API_USER;
+exports.API_RBAC = API_RBAC;
+exports.PASSWORD = PASSWORD;
