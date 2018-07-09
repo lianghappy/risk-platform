3a369eff87e6ed3b302775cfa3b7cf2d1f35e6cb
JIMI <JIMI@JIMIdeMacBook-Pro.local>
2018-07-09 11:01:22 +0800

    规则树查询报错

--- src/routes/sandboxie/sandbox/regular/RegularModal.js
+++ src/routes/sandboxie/sandbox/regular/RegularModal.js
@@ -149,6 +149,15 @@ export default class RegularModal extends React.PureComponent {
         const {
             form,
         } = this.props;
+        if (selectedKeys.length === 0) {
+            this.setState({ selectedKeys: [], categorieId: '' });
+            this.props.form.resetFields();
+            this.query({
+                pageNum: 1,
+                pageSize: 5,
+            });
+            return;
+        }
         this.setState({ categorieId: selectedKeys[0].substring(selectedKeys[0].indexOf('$') + 1) });
         const categoryId = selectedKeys[0].substring(selectedKeys[0].indexOf('$') + 1);
         form.validateFields((errors, values) => {
@@ -184,9 +193,11 @@ export default class RegularModal extends React.PureComponent {
                 pageSize: 5,
             });
         } else {
+            const categoryId = this.state.categorieId;
             this.query({
                 pageNum: 1,
                 pageSize: 5,
+                categoryId,
             });
         }
     };
@@ -202,10 +213,12 @@ export default class RegularModal extends React.PureComponent {
                     pageSize: 5,
                 });
             } else {
+                const categoryId = this.state.categorieId;
                 this.query({
                     ...values,
                     pageNum: 1,
                     pageSize: 5,
+                    categoryId,
                 });
             }
         });
